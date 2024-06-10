0ea642160e74e9b001ce5b733e5ff189cfc98745
Moisés Madeira <msalcmd@gmail.com>
2024-06-10 14:35:44 -0300

    added scripts

---
+++ scripts/consulta.sql
@@ -0,0 +1,7 @@
+USE BANCO_DADOS_IT_TALENT;
+
+-- Consultar documentos na tabela
+SELECT * FROM ALUNOS;
+
+SELECT default_character_set_name FROM information_schema.SCHEMATA 
+WHERE schema_name = "BANCO_DADOS_IT_TALENT";
